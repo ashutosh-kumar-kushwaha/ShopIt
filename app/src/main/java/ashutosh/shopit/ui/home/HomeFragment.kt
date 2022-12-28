be3d16107bfa1c5d6a40775cf61cce2d89d73830
ashutosh-kumar-kushwaha <akkushwaha0104@gmail.com>
2022-12-28 17:10:17 +0530

    Add Search View

--- app/src/main/java/ashutosh/shopit/ui/home/HomeFragment.kt
+++ app/src/main/java/ashutosh/shopit/ui/home/HomeFragment.kt
@@ -1,19 +1,58 @@
 package ashutosh.shopit.ui.home
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.appcompat.widget.SearchView
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+import androidx.fragment.app.Fragment
 import ashutosh.shopit.R
+import java.lang.reflect.Field
 
 class HomeFragment : Fragment() {
 
+
+
+    @SuppressLint("DiscouragedPrivateApi")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+
+        val searchView = view.findViewById<SearchView>(R.id.searchView)
+
+        val searchText = searchView.findViewById<EditText>(androidx.appcompat.R.id.search_src_text)
+        val font = ResourcesCompat.getFont(requireContext(), R.font.montserrat)
+        searchText.typeface = font
+        searchText.setTextColor(ContextCompat.getColor(requireContext(), R.color.color2))
+        searchText.setHintTextColor(ContextCompat.getColor(requireContext(), R.color.color2))
+
+        if(Build.VERSION.SDK_INT >= 29){
+            searchText.textCursorDrawable = AppCompatResources.getDrawable(requireContext(), R.drawable.cursor_2)
+        }
+        else{
+            try{
+                val f: Field = TextView::class.java.getDeclaredField("mCursorDrawableRes")
+                f.isAccessible = true
+                f.set(searchText, R.drawable.cursor_2)
+            }
+            catch (_: Exception){}
+        }
+
+
+        val icon = searchView.findViewById<ImageView>(androidx.appcompat.R.id.search_mag_icon)
+        icon.setImageResource(R.drawable.ic_search_icon)
+
+        return view
     }
 }
