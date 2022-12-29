d5b68bdc4be03aa9e498c0ea21350dd1fd46475d
ashutosh-kumar-kushwaha <akkushwaha0104@gmail.com>
2022-12-30 03:01:19 +0530

    Add indicator of viewpager

--- app/src/main/java/ashutosh/shopit/ui/home/HomeFragment.kt
+++ app/src/main/java/ashutosh/shopit/ui/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package ashutosh.shopit.ui.home
 
 import android.annotation.SuppressLint
+import android.media.Image
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,20 +9,21 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.viewpager2.widget.ViewPager2
 import ashutosh.shopit.R
 import ashutosh.shopit.adapters.OffersAdapter
 import ashutosh.shopit.adapters.ProductSpacingItemDecoration
 import ashutosh.shopit.adapters.ProductsAdapter
 import ashutosh.shopit.databinding.FragmentHomeBinding
-import ashutosh.shopit.models.Product
-import ashutosh.shopit.ui.auth.getStarted.GetStartedViewModel
 import java.lang.reflect.Field
 
 class HomeFragment : Fragment() {
@@ -31,6 +33,9 @@ class HomeFragment : Fragment() {
 
     private val homeViewModel by viewModels<HomeViewModel>()
 
+    private val productsAdapter = ProductsAdapter()
+
+    private lateinit var circles : List<ImageView>
 
     @SuppressLint("DiscouragedPrivateApi")
     override fun onCreateView(
@@ -39,6 +44,9 @@ class HomeFragment : Fragment() {
     ): View? {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = homeViewModel
 
         val searchText = binding.searchView.findViewById<EditText>(androidx.appcompat.R.id.search_src_text)
         val font = ResourcesCompat.getFont(requireContext(), R.font.montserrat)
@@ -61,16 +69,66 @@ class HomeFragment : Fragment() {
         val icon = binding.searchView.findViewById<ImageView>(androidx.appcompat.R.id.search_mag_icon)
         icon.setImageResource(R.drawable.ic_search_icon)
 
-        binding.offersViewPager.adapter = OffersAdapter(listOf(R.drawable.offer, R.drawable.offer, R.drawable.offer, R.drawable.offer, R.drawable.offer, R.drawable.offer))
+        binding.offersViewPager.adapter = OffersAdapter(homeViewModel.offersList.value!!)
 
         binding.itemRecyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
-        val adapter = ProductsAdapter()
-        adapter.submitList(listOf(Product(1, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490"), Product(2, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490"), Product(3, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490"), Product(4, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490"), Product(5, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490"), Product(6, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490"), Product(7, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490"), Product(8, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490"), Product(9, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490"), Product(10, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490")))
-        binding.itemRecyclerView.adapter = adapter
+
+        binding.itemRecyclerView.adapter = productsAdapter
         binding.itemRecyclerView.addItemDecoration(ProductSpacingItemDecoration(2, resources.getDimensionPixelSize(R.dimen.dp_21), resources.getDimensionPixelSize(R.dimen.dp_8)))
 
+        circles =  listOf(binding.circle1, binding.circle2, binding.circle3, binding.circle4, binding.circle5, binding.circle6)
+
+        binding.offersViewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageScrollStateChanged(state: Int) {
+                super.onPageScrollStateChanged(state)
+
+                val currentItem = binding.offersViewPager.currentItem
+
+                selectItem(currentItem)
+                unselectItem(currentItem-1)
+                unselectItem(currentItem+1)
+
+            }
+        })
 
         return binding.root
+    }
+
+    private fun selectItem(i: Int){
+        var index = i
+        if(index < 0) index+=6
+        if(index > 5) index-=6
+        val imageView = circles[index]
+        imageView.setImageResource(R.drawable.viewpager_selected)
+        val lp = LinearLayout.LayoutParams(resources.getDimensionPixelSize(R.dimen.dp_16), resources.getDimensionPixelSize(R.dimen.dp_5))
+        lp.marginStart = resources.getDimensionPixelSize(R.dimen.dp_2)
+        imageView.layoutParams = lp
+    }
+
+    private fun unselectItem(i: Int){
+        var index = i
+        if(index < 0) index+=6
+        if(index > 5) index-=6
+        val imageView = circles[index]
+        imageView.setImageResource(R.drawable.viewpager_not_selected)
+        val lp = LinearLayout.LayoutParams(resources.getDimensionPixelSize(R.dimen.dp_5), resources.getDimensionPixelSize(R.dimen.dp_5))
+        lp.marginStart = resources.getDimensionPixelSize(R.dimen.dp_2)
+        imageView.layoutParams = lp
+    }
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        homeViewModel.productsList.observe(viewLifecycleOwner, Observer{
+            productsAdapter.submitList(it)
+        })
+
+        homeViewModel.offersList.observe(viewLifecycleOwner, Observer{
+            binding.offersViewPager.adapter = OffersAdapter(it)
+        })
+
     }
 
     override fun onDestroyView() {
--- app/src/main/java/ashutosh/shopit/ui/home/HomeViewModel.kt
+++ app/src/main/java/ashutosh/shopit/ui/home/HomeViewModel.kt
@@ -1,9 +1,19 @@
 package ashutosh.shopit.ui.home
 
 import androidx.lifecycle.ViewModel
+import ashutosh.shopit.R
+import ashutosh.shopit.SingleLiveEvent
+import ashutosh.shopit.models.Product
 
 class HomeViewModel : ViewModel() {
 
+    val productsList = SingleLiveEvent<List<Product>>()
 
+    val offersList = SingleLiveEvent<List<Int>>()
+
+    init {
+        productsList.value = listOf(Product(1, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490"), Product(2, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490"), Product(3, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490"), Product(4, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490"), Product(5, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490"), Product(6, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490"), Product(7, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490"), Product(8, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490"), Product(9, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490"), Product(10, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490"))
+        offersList.value = listOf(R.drawable.offer, R.drawable.offer, R.drawable.offer, R.drawable.offer, R.drawable.offer, R.drawable.offer)
+    }
 
 }
