e4fc0378a13309fcf6ee7300aecb0a2479206a49
ashutosh-kumar-kushwaha <akkushwaha0104@gmail.com>
2023-01-05 14:54:47 +0530

    Changed product list to MutableLiveData

--- app/src/main/java/ashutosh/shopit/ui/home/HomeViewModel.kt
+++ app/src/main/java/ashutosh/shopit/ui/home/HomeViewModel.kt
@@ -1,5 +1,6 @@
 package ashutosh.shopit.ui.home
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ashutosh.shopit.R
 import ashutosh.shopit.SingleLiveEvent
@@ -7,9 +8,9 @@ import ashutosh.shopit.models.Product
 
 class HomeViewModel : ViewModel() {
 
-    val productsList = SingleLiveEvent<List<Product>>()
+    val productsList = MutableLiveData<List<Product>>()
 
-    val offersList = SingleLiveEvent<List<Int>>()
+    val offersList = MutableLiveData<List<Int>>()
 
     init {
         productsList.value = listOf(Product(1, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490"), Product(2, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490"), Product(3, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490"), Product(4, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490"), Product(5, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490"), Product(6, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490"), Product(7, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490"), Product(8, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490"), Product(9, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490"), Product(10, R.drawable.iphone, "Apple Iphone 14 128GB (Product) RED", "4.5", 1335, "₹ 77,490", "₹ 77,490"))
