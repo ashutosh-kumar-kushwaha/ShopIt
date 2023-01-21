dc8281366783c148533de9b1ef4410ab35a7f1b5
ashutosh-kumar-kushwaha <akkushwaha0104@gmail.com>
2023-01-21 17:45:40 +0530

    Fix Reset Password

--- app/src/main/java/ashutosh/shopit/ui/auth/resetPassword/ResetPasswordViewModel.kt
+++ app/src/main/java/ashutosh/shopit/ui/auth/resetPassword/ResetPasswordViewModel.kt
@@ -1,5 +1,6 @@
 package ashutosh.shopit.ui.auth.resetPassword
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ashutosh.shopit.SingleLiveEvent
@@ -16,25 +17,20 @@ class ResetPasswordViewModel(val email : String, val otp : String) : ViewModel()
     val password2LiveData = MutableLiveData("")
 
     suspend fun resetPassword(){
-        if(password1LiveData.value != null && password2LiveData.value != null){
-            if(password1LiveData.value == password2LiveData.value){
-                if(password1LiveData.value!!.length < 8){
-                    if(password1LiveData.value!!.matches(Regex("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$"))){
-                        resetPasswordRepository.resetPassword(email, otp, password1LiveData.value!!)
-                    }
-                    else{
-                        errorMessage.value = "Password must contain at least one uppercase letter, one lowercase letter, one numeric character, one special character and no spaces"
-                    }
+        if (password1LiveData.value != null && password2LiveData.value != null) {
+            if (password1LiveData.value == password2LiveData.value) {
+                if (password1LiveData.value!!.matches(Regex("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$"))) {
+                    resetPasswordRepository.resetPassword(email, otp, password1LiveData.value!!)
+                } else {
+                    errorMessage.value =
+                        "Password must contain at least one uppercase letter, one lowercase letter, one numeric character, one special character and no spaces and must have at least 8 characters"
                 }
-                else{
-                    errorMessage.value = "Password must contain at least 8 characters"
-                }
-            }
-            else{
+            } else {
                 errorMessage.value = "Enter same passwords in both fields"
+                Log.d("Ashu", password1LiveData.value!!)
+                Log.d("Ashu", password2LiveData.value!!)
             }
-        }
-        else{
+        } else {
             errorMessage.value = "Enter passwords in both fields"
         }
     }
