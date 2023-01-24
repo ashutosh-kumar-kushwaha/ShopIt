99dfdb5265f857a60efcf881a6aeba830765a3bb
ashutosh-kumar-kushwaha <akkushwaha0104@gmail.com>
2023-01-24 15:03:26 +0530

    Add Add Address UI

---
+++ app/src/main/java/ashutosh/shopit/ui/address/AddAddressFragment.kt
@@ -0,0 +1,20 @@
+package ashutosh.shopit.ui.address
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ashutosh.shopit.R
+
+class AddAddressFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_address, container, false)
+    }
+
+}
