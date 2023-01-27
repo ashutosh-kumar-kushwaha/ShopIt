f31e323eb175b92a2afd4f2348b40947fccf1cf4
ashutosh-kumar-kushwaha <akkushwaha0104@gmail.com>
2023-01-27 18:59:38 +0530

    Add Image Adapter

---
+++ app/src/main/java/ashutosh/shopit/adapters/ImageAdapter.kt
@@ -0,0 +1,36 @@
+package ashutosh.shopit.adapters
+
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil.ItemCallback
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import ashutosh.shopit.databinding.ImageItemBinding
+
+class ImageAdapter : ListAdapter<Uri, ImageAdapter.ViewHolder>(DiffUtil()) {
+    inner class ViewHolder(private val binding: ImageItemBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(uri: Uri){
+            binding.imageView.setImageURI(uri)
+        }
+    }
+
+    class DiffUtil: ItemCallback<Uri>(){
+        override fun areItemsTheSame(oldItem: Uri, newItem: Uri): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Uri, newItem: Uri): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(ImageItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
--- app/src/main/java/ashutosh/shopit/ui/review/ReviewFragment.kt
+++ app/src/main/java/ashutosh/shopit/ui/review/ReviewFragment.kt
@@ -5,17 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import ashutosh.shopit.R
+import ashutosh.shopit.databinding.FragmentReviewBinding
 
 class ReviewFragment : Fragment() {
+
+    private var _binding : FragmentReviewBinding? = null
+    private val binding: FragmentReviewBinding get() = _binding!!
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_review, container, false)
+        _binding = FragmentReviewBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
